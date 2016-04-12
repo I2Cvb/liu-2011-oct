d10b8ed91229b3a6b7fc9e00fa203f741f12bb0f
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-04-12 21:57:37 +0200

    fixed the preprocessing

--- pipeline/feature-preprocessing/pipeline_preprocessing.m
+++ pipeline/feature-preprocessing/pipeline_preprocessing.m
@@ -10,22 +10,18 @@ x_size = 512;
 y_size = 128;
 z_size = 1024;
 
-% Define a fix sigma value
-method_denoising = 'bm3d';
-sigma = 190. / 255.;
-
 % Define the method for flattening
-method_flattening = 'srinivasan-2014';
+method_flattening = 'liu-2011';
 
 % Define the parameters for the cropping
 method_cropping = 'srinivasan-2014';
-h_over_rpe = 325;
-h_under_rpe = 30;
-width_crop = 340;
+h_over_rpe = 200;
+h_under_rpe = 200;
+width_crop = x_size;
 
 % Define the data directory
 data_directory = '/data/retinopathy/OCT/SERI/original_data/';
-store_directory = '/data/retinopathy/OCT/SERI/pre_processed_data/srinivasan_2014/';
+store_directory = '/data/retinopathy/OCT/SERI/pre_processed_data/liu_2011/';
 directory_info = dir(data_directory);
 
 poolobj = parpool('local', 40);
@@ -46,11 +42,10 @@ for idx_file = 1:size(directory_info)
         vol = vol / max( vol(:) );
 
         % Apply the preprocessing
-        vol_denoised = denoising_volume( vol, method_denoising, sigma );
-        disp( [ 'Image ', directory_info(idx_file).name, ' denoised' ] ...
-             );
+        tic
         [ baseline_vol, vol_flattened ] = flattening_volume( ...
-            vol_denoised, method_flattening );
+            vol, method_flattening, 'otsu', false );
+        toc
         disp( [ 'Image ', directory_info(idx_file).name, ' flattened' ] ...
              );
         vol_cropped = crop_volume( vol_flattened, method_cropping, ...
@@ -63,6 +58,7 @@ for idx_file = 1:size(directory_info)
         store_filename = strcat( store_directory, strrep( ...
             directory_info(idx_file).name, '.img', '.mat' ) );
         save( store_filename, 'vol_cropped' );
+        % save( store_filename, 'vol_flattened' );
         disp( [ 'Image ', directory_info(idx_file).name, ' cropped ' ...
                             'was stored' ] );
 
